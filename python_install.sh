e5c5f5f8250c1f4ac0d77bd78ae6a76b796a6df2
Charles Reid <charlesreid1@gmail.com>
2018-01-17 04:51:54 -0800

    Adding scripts to set up brew and software tooling environment

---
+++ python_install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$(id -u)" == "0" ]; then
+    echo "DO NOT RUN THIS AS ROOT" 1>&2
+    exit 1;
+fi
+
+pip2 install --user numpy scipy pandas
+pip3 install --user numpy scipy pandas
+
+pip2 install --user matplotlib seaborn
+pip3 install --user matplotlib seaborn
+
+pip2 install --user jupyter ipython
+pip3 install --user jupyter ipython
+
+pip2 install --user tornado pyzmq pygments
+pip3 install --user tornado pyzmq pygments
+
+pip2 install --user pygments pillow pelican
+pip3 install --user pygments pillow pelican
+
---
+++ python_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+brew install python2 python3
+
