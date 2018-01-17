d57063f418cdab31ecf8b022a5c79ccb8b7eea02
Charles Reid <charlesreid1@gmail.com>
2018-01-17 05:32:20 -0800

    Adding -U flag to pip install commands

--- python_install.sh
+++ python_install.sh
@@ -5,18 +5,18 @@ if [ "$(id -u)" == "0" ]; then
     exit 1;
 fi
 
-pip2 install --user numpy scipy pandas
-pip3 install --user numpy scipy pandas
-
-pip2 install --user matplotlib seaborn
-pip3 install --user matplotlib seaborn
-
-pip2 install --user jupyter ipython
-pip3 install --user jupyter ipython
-
-pip2 install --user tornado pyzmq pygments
-pip3 install --user tornado pyzmq pygments
-
-pip2 install --user pygments pillow pelican
-pip3 install --user pygments pillow pelican
+pip2 install -U --user numpy scipy pandas
+pip3 install -U --user numpy scipy pandas
+                
+pip2 install -U --user matplotlib seaborn
+pip3 install -U --user matplotlib seaborn
+                
+pip2 install -U --user jupyter ipython
+pip3 install -U --user jupyter ipython
+                
+pip2 install -U --user tornado pyzmq pygments
+pip3 install -U --user tornado pyzmq pygments
+                
+pip2 install -U --user pygments pillow pelican
+pip3 install -U --user pygments pillow pelican
 
